9efea931d3d5547a1d1ee626242bdb6b852ee4cd
amudalamadhusudhan <amadhusudhan2016@gmail.com>
2022-08-17 23:56:27 +0530

    priority

---
+++ priority/main/cdacmain.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "pthread.h"
+TaskHandle_t serial_task_handle;
+void serial_task(void *data)
+{
+    while (1)
+    {
+        UBaseType_t prio;
+        prio = uxTaskPriorityGet(serial_task_handle);
+        printf("serial task started %d\n", prio);
+    }
+    vTaskDelete(serial_task_handle);
+}
+void app_main()
+{
+    BaseType_t res;
+
+    printf("MT rtos project\n");
+    // pthread_t sid;
+    // pthread_create(&sid, NULL, serial_task, NULL);
+    // pthread_join(sid, NULL);
+    res = xTaskCreate(serial_task, "SERIALTASK", 1024, NULL, 5, &serial_task_handle);
+    if (res == pdPASS)
+    {
+        printf("MT task created sucussfully\n");
+    }
+}
