ebc7b8ad5af389e9825a5445a84afd94658ed8bf
amudalamadhusudhan <amadhusudhan2016@gmail.com>
2022-08-18 22:22:45 +0530

    tasksuspend

--- tasksuspend/main/cdacmain.c
+++ tasksuspend/main/cdacmain.c
@@ -7,11 +7,16 @@ TaskHandle_t ether_task_handle;
 void serial_task(void *data)
 {
     UBaseType_t prio;
-
+    int count = 0;
     while (1)
     {
-
-        prio = uxTaskPriorityGet(NULL);
+        count += 1;
+        if (count > 5)
+        {
+            vTaskResume(ether_task_handle);
+            count = 0;
+        }
+        prio = uxTaskPriorityGet(ether_task_handle);
         printf("serial task started %d\n", prio);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
@@ -22,10 +27,10 @@ void ether_task(void *data)
     UBaseType_t prioe;
     while (1)
     {
-        // vTaskSuspend(NULL);
-        prioe = uxTaskPriorityGet(NULL);
+        vTaskSuspend(NULL);
+        prioe = uxTaskPriorityGet(ether_task_handle);
         printf("ether task started %d\n", prioe);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        // vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
     vTaskDelete(ether_task_handle);
 }
@@ -36,6 +41,6 @@ void app_main()
     printf("MT rtos project \n");
     priom = uxTaskPriorityGet(NULL);
     printf("MT task created sucessfully prio %d\n", priom);
-    res = xTaskCreate(&serial_task, "SERIALTASK", 2048, NULL, 5, &serial_task_handle);
-    res = xTaskCreate(&ether_task, "ETHERTASK", 2048, NULL, 10, &ether_task_handle);
+    res = xTaskCreate(serial_task, "SERIALTASK", 2048, NULL, 5, &serial_task_handle);
+    res = xTaskCreate(ether_task, "ETHERTASK", 2048, NULL, 10, &ether_task_handle);
 }
