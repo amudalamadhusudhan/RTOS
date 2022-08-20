f6ffb90dfca84385394f2c43ddaef0d54ca7f126
amudalamadhusudhan <amadhusudhan2016@gmail.com>
2022-08-20 17:40:55 +0530

    streambuufer

---
+++ streambuffer/main/cdacmain.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "pthread.h"
+#include "freertos/timers.h"
+#include "freertos/message_buffer.h"
+
+TaskHandle_t task1handle,task2handle, task3handle, task4handle;
+StreamBufferHandle_t sb_t1_t2;
+
+void task1(void *data)
+{
+  printf("task1 sarting the task1\n");
+  printf("task1 sending message\n");
+  //while(1)
+  //{
+  xStreamBufferSend(sb_t1_t2,"madhusudhan1\n",13,pdMS_TO_TICKS(20000));
+  xStreamBufferSend(sb_t1_t2,"madhusudhan2\n",13,pdMS_TO_TICKS(20000));
+  xStreamBufferSend(sb_t1_t2,"madhusudhan3\n",13,pdMS_TO_TICKS(20000));
+  printf("task1 finished sending the message\n");
+  //}
+  vTaskDelete(NULL);
+}
+void task2(void *data)
+{
+  unsigned char rbuff[200];
+  size_t nbytes=0;
+  printf("task2 sarting the task2\n");
+  while(1)
+  {
+  printf("task2 requesting for data to be received\n");
+  nbytes=xStreamBufferReceive(sb_t1_t2,rbuff,200,pdMS_TO_TICKS(20000));
+  printf("task2 recived the data and size %s %d\n",rbuff,nbytes);
+  }
+  vTaskDelete(NULL);
+}
+
+
+void app_main()
+{
+  BaseType_t res;
+  UBaseType_t priom;
+  printf("MT rtos project \n");
+  priom = uxTaskPriorityGet(NULL);
+  printf("MT task created sucessfully prio %d\n", priom);
+  sb_t1_t2= xStreamBufferCreate(200,25);
+  res = xTaskCreate(task1, "TASK1", 2048, NULL, 8, &task1handle);
+  res = xTaskCreate(task2, "TASK2", 2048, NULL, 8, &task2handle);
+}
